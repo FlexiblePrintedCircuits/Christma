31d2a2ef1a4f2ed90b213544df726b6fc5213c3a
Iwancof <rock.iwancof@gmail.com>
2019-12-25 04:26:49 +0900

    image fix

--- read.py
+++ read.py
@@ -38,14 +38,64 @@ for y in range(size[1]) :
             #print(opecode[value], end = "")
             program += opecode[value]
 
-import subprocess
+mem = [0 for i in range(10000)]
+pointer = 0
+program_counter = 0
 
-f = open("tmp.chri","w")
-f.write(program)
-f.close()
+# コマンドライン引数からファイルを取得
+tape = program
 
-subprocess.call("a.exe tmp.chri")
+while program_counter < len(tape):
+    # C
+    if tape[program_counter] == 'C':
+        pointer += 1
 
+    # H
+    elif tape[program_counter] == 'H':
+        pointer -= 1
 
+    # R
+    elif tape[program_counter] == 'R':
+        mem[pointer] += 1
+
+    # I
+    elif tape[program_counter] == 'I':
+        mem[pointer] -= 1
+
+    # S
+    elif tape[program_counter] == 'S':
+        print(chr(mem[pointer]), end="")
+
+    # T
+    elif tape[program_counter] == 'T':
+        mem[pointer] = int(input())
+
+    # M
+    elif tape[program_counter] == 'M':
+        if mem[pointer] == 0:
+            nest = 0
+            while True:
+                program_counter += 1
+                if tape[program_counter] == 'M':
+                    nest += 1
+                if tape[program_counter] == 'A' and nest == 0:
+                    break
+                if tape[program_counter] == 'A':
+                    nest -= 1
+
+    # A
+    elif tape[program_counter] == 'A':
+        if mem[pointer] != 0:
+            nest = 0
+            while True:
+                program_counter -= 1
+                if tape[program_counter] == 'A':
+                    nest += 1
+                if tape[program_counter] == 'M' and nest == 0:
+                    break
+                if tape[program_counter] == 'M':
+                    nest -= 1
+
+    program_counter += 1
 
         
