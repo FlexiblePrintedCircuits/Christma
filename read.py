b3a3e3a3d6d1af802618312fb3c562c2f6cb23ff
Iwancof <rock.iwancof@gmail.com>
2019-12-25 02:26:06 +0900

    steg fix and add funcs

--- read.py
+++ read.py
@@ -3,10 +3,13 @@ import os
 from PIL import Image
 import math
 
-def from_binary(d) :
-    return d[2] * 4 + d[1] * 2 + d[0]
+opecode = "><+-.,[]"
 
-img = Image.open("tmp.png")
+def from_binary(d) :
+    return d[2] * 9 + d[1] * 3 + d[0]
+
+#img = Image.open("couple_origin.png_steged.png")
+img = Image.open(sys.argv[1])
 rgba_img = img.convert('RGBA')
 size = rgba_img.size
 
@@ -18,17 +21,18 @@ for y in range(size[1]) :
     for x in range(size[0]) :
         r,g,b,a = rgba_img.getpixel((x,y))
 
-        if(index > 50) :
-            flag = False
+        x = [0] * 3
+        x[2] = r % 3
+        x[1] = g % 3
+        x[0] = b % 3
 
-        index += 1
+        value = from_binary(x)
+    
+        if(value == 26) :
+            flag = False
         
         if(flag) :
-            x = [0] * 3
-            x[2] = r % 2
-            x[1] = g % 2
-            x[0] = b % 2
-            print(from_binary(x))
+            print(opecode[value])
 
 
         
