9861a9ee37640c46b0d20e015f3f2c9d1987409d
kakao <kwt20018@gmail.com>
2019-12-25 02:11:14 +0900

    とりま

--- chri_py.py
+++ chri_py.py
@@ -1,5 +1,3 @@
-import sys
-
 # C -ポインタをインクリメント【 (p++) 】
 # H -ポインタをデクリメント 【 (p--) 】
 # R -ポインタの値をインクリメン ト【 (*p)++) 】
@@ -8,3 +6,64 @@ import sys
 # T -入力から1バイト読み込んでポインタが指す値に代入 【 *p = get() 】
 # M -ポインタの指す値が0なら後のAまでジャンプ 【 if(*p == 0){ goto back } 】
 # A -ポインタの指す値が0でなければ前のMまでジャンプ 【　if(*p == 0) { goto forward }　】
+
+file_path = "./brain_fuck_code.txt"
+
+mem = [0 for i in range(10000)]
+pointer = 0
+program_counter = 0
+
+tape = open(file_path, "r").read()
+
+while program_counter < len(tape):
+    # C
+    if tape[program_counter] == 'C':
+        pointer += 1
+
+    # H
+    elif tape[program_counter] == 'H':
+        pointer -= 1
+
+    # R
+    elif tape[program_counter] == 'R':
+        mem[pointer] += 1
+
+    # I
+    elif tape[program_counter] == 'I':
+        mem[pointer] -= 1
+
+    # S
+    elif tape[program_counter] == 'S':
+        print(chr(mem[pointer]), end="")
+
+    # T
+    elif tape[program_counter] == 'T':
+        mem[pointer] = int(input())
+
+    # M
+    elif tape[program_counter] == 'M':
+        if mem[pointer] == 0:
+            nest = 0
+            while True:
+                program_counter += 1
+                if tape[program_counter] == '[':
+                    nest += 1
+                if tape[program_counter] == ']' and nest == 0:
+                    break
+                if tape[program_counter] == ']':
+                    nest -= 1
+
+    # A
+    elif tape[program_counter] == 'A':
+        if mem[pointer] != 0:
+            nest = 0
+            while True:
+                program_counter -= 1
+                if tape[program_counter] == ']':
+                    nest += 1
+                if tape[program_counter] == '[' and nest == 0:
+                    break
+                if tape[program_counter] == '[':
+                    nest -= 1
+
+    program_counter += 1
